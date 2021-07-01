f5d98a5841d2eedb24a05bf68bba1ae90af764f5
Karenrodriguezlab <84875960+Karenrodriguezlab@users.noreply.github.com>
2021-07-01 16:30:57 -0400

    Update deploy-prod.sh

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -2,7 +2,7 @@
 #Despliegue a pruebasCI
 echo "Converting to MDAPI format..."
 sfdx force:org:list
-echo "Command - sfdx force:mdapi:retrieve -r packageRoles -u my-hub-orgPruebasCI -k packageRoles/package.xml"
+#echo "Command - sfdx force:mdapi:retrieve -r packageRoles -u my-hub-orgPruebasCI -k packageRoles/package.xml"
 sfdx force: mdapi:convert -d deploy_prod -r pruebaPackage
 sfdx config:set defaultusername=consultor_force+amicar@nectia.com.pruebasci
 sfdx force:mdapi:deploy -c -d pruebaPackage -u my-hub-orgPruebasCI -w 10 #-l RunSpecifiedTests -r AMICAR_ChecklistGetJSONTest -w 33 --verbose --loglevel fatal
