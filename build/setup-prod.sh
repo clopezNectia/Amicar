efebb467ba0aa25107afa926a69c3dacd931d0bc
Karenrodriguezlab <84875960+Karenrodriguezlab@users.noreply.github.com>
2021-07-01 16:24:54 -0400

    Update setup-prod.sh

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -9,6 +9,6 @@ sfdx auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key -
 echo "Conectar Dodoria"
 sfdx alias:set my-hub-Dodoria=consultor_force+amicar@nectia.com.sbxdodoria
 sfdx auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER --instanceurl https://test.salesforce.com
-echo "Conectar Prod"
-sfdx alias:set my-hub-Prod=consultor_force+amicar@nectia.com
-sfdx auth:jwt:grant --clientid $SFDC_PROD_CLIENTID1 --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER1 --instanceurl https://login.salesforce.com
+#echo "Conectar Prod"
+#sfdx alias:set my-hub-Prod=consultor_force+amicar@nectia.com
+#sfdx auth:jwt:grant --clientid $SFDC_PROD_CLIENTID1 --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER1 --instanceurl https://login.salesforce.com
