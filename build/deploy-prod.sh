52f6e4da728c6af0b9995f7d112b04c15f9f8e04
Karenrodriguezlab <84875960+Karenrodriguezlab@users.noreply.github.com>
2021-07-01 17:45:16 -0400

    Update deploy-prod.sh

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -7,7 +7,7 @@ sfdx force:org:list
 #sfdx config:set defaultusername=consultor_force+amicar@nectia.com.pruebasci
 #sfdx force:mdapi:deploy -d pruebaPackage -u my-hub-orgPruebasCI -w 10 #-l RunSpecifiedTests -r AMICAR_ChecklistGetJSONTest -w 33 --verbose --loglevel fatal
 
-Despliegue producción
+#Despliegue producción
 #echo "Command - sfdx force:mdapi:retrieve -r packageRoles -u my-hub-Prod -k packageRoles/package.xml"
 sfdx force: mdapi:convert -d deploy_prod -r pruebaPackage
 sfdx config:set defaultusername=consultor_force+amicar@nectia.com
